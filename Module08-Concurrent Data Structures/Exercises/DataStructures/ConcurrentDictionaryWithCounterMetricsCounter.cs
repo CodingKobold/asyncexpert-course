e9dae3880add68e93d7d116389ee3e2d9b7785f2
Szymon Kulec <scooletz@gmail.com>
2020-07-30 22:16:25 +0200

    answers for Homework module 08

--- Module08-Concurrent Data Structures/Exercises/DataStructures/ConcurrentDictionaryWithCounterMetricsCounter.cs
+++ Module08-Concurrent Data Structures/Exercises/DataStructures/ConcurrentDictionaryWithCounterMetricsCounter.cs
@@ -1,22 +1,32 @@
 ﻿using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace DataStructures
 {
     public class ConcurrentDictionaryWithCounterMetricsCounter : IMetricsCounter
     {
-        // Implement this class using ConcurrentDictionary and the provided AtomicCounter class.
-        // AtomicCounter should be created only once per key, then its Increment method should be used.
+        readonly ConcurrentDictionary<string, AtomicCounter> counters = new ConcurrentDictionary<string, AtomicCounter>();
         
         public IEnumerator<KeyValuePair<string, int>> GetEnumerator()
         {
-            throw new System.NotImplementedException();
+            return counters.Select(kvp => new KeyValuePair<string, int>(kvp.Key, kvp.Value.Count)).GetEnumerator();
         }
 
         public void Increment(string key)
         {
-            throw new System.NotImplementedException();
+            // Getting value uses lock-free, it might be potentially faster but we should benchmark it to make sure that this is the case.
+            if (counters.TryGetValue(key, out var atomic))
+            {
+                atomic.Increment();
+            }
+            else
+            {
+                // failed to get, ensure that the counter is created and incremented only once
+                counters.AddOrUpdate(key, k => new AtomicCounter(), (_, existing) => existing).Increment();
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
